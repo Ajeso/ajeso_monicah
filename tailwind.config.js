2e4c662d4d012ecf4ba9b33d8c513b6f17d72256
Monicah Ajeso <96474976+Ajeso@users.noreply.github.com>
2023-07-25 16:10:30 +0200

    added the animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
+  mode: "jit",
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
         Montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        Roboto: ["Roboto", ...defaultTheme.fontFamily.serif],
       },
       colors: {
         nude: "#ece7e1",
