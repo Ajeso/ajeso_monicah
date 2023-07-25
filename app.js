178efa411723fe8d831e5269d654096461dd239e
Monicah Ajeso <96474976+Ajeso@users.noreply.github.com>
2023-07-25 20:29:24 +0200

    modal window
    added the set timeout  to remove the delay during mouseover

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 "use strict";
-
 const projectThumbnails = document.querySelectorAll(".project-thumbnail");
 const modal = document.getElementById("project-modal");
 const closeModal = document.getElementById("close-modal");
+let isModalOpen = false;
+let hideTimer;
 
 function showModal(projectId, thumbnail) {
+  if (isModalOpen) return;
   const projectDetails = document.getElementById(`${projectId}-details`);
   if (projectDetails) {
     modal.style.display = "block";
@@ -17,27 +19,41 @@ function showModal(projectId, thumbnail) {
     const modalContent = modal.querySelector(".modal-content");
     modalContent.style.left = `${thumbnailX}px`;
     modalContent.style.top = `${thumbnailY}px`;
+
+    isModalOpen = true;
   }
 }
 
 function hideModal() {
-  modal.style.display = "none";
-  const projectDetails = document.querySelectorAll(".project-details");
-  projectDetails.forEach((project) => {
-    project.style.display = "none";
-  });
+  hideTimer = setTimeout(() => {
+    modal.style.display = "none";
+    const projectDetails = document.querySelectorAll(".project-details");
+    projectDetails.forEach((project) => {
+      project.style.display = "none";
+    });
+    isModalOpen = false;
+  }, 20);
 }
 
 projectThumbnails.forEach((thumbnail) => {
   const projectId = thumbnail.dataset.project;
 
   thumbnail.addEventListener("mouseenter", () => {
+    clearTimeout(hideTimer);
     showModal(projectId, thumbnail);
   });
 
   thumbnail.addEventListener("mouseleave", () => {
     hideModal();
   });
+});
+
+modal.addEventListener("mouseenter", () => {
+  clearTimeout(hideTimer);
+});
+
+modal.addEventListener("mouseleave", () => {
+  hideModal();
 });
 
 closeModal.addEventListener("click", () => {
